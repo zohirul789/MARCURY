41764b1653d2d620cd147ac367c1a27a2c3d800a
ALPR <ALPR@192.168.0.32>
2017-05-16 03:11:33 -0400

    Wevdriver

---
+++ WebDriver/src/TestDriver.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver ();
+		driver.get("https://www.yahoo.com/");
+		
+
+	}
+
+}
